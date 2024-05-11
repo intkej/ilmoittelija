9c4f16072216f48146b26688905a36a88e42c94f
Johannes Intke <124790333+intkej@users.noreply.github.com>
2024-05-11 19:07:53 +0300

    Add support for multiple different media links

--- src/index.js
+++ src/index.js
@@ -1,28 +1,38 @@
 require('dotenv').config();
 const Parser = require("rss-parser");
 const utils = require('./utils/utils');
-var time;
 let parser = new Parser();
 
-function check_news() {
-  parser.parseURL(process.env.NEWS_URL, function(err, feed) {
+// using a JS array for now
+const media_links = ['https://www.iltalehti.fi/rss/uutiset.xml', 'https://feeds.yle.fi/uutiset/v1/recent.rss?publisherIds=YLE_UUTISET', 'https://www.hs.fi/rss/tuoreimmat.xml']
+
+let latest_news = {} // lets use the object method instead of time-method
+
+function check_news(link) {
+    parser.parseURL(link, function(err, feed) { 
     if (err) {
-      console.log("Error while fetching RSS feed:", err)
+      console.log("Error while fetchinng RSS feed:", err); return;
     }
     const feed_items = feed.items[0];
-    if (time !== feed_items.pubDate) {
+    if (!latest_news[link] || latest_news[link] !== feed_items.pubDate) {
       utils.send_sms(feed_items.title, feed_items.link);
-      time = feed_items.pubDate;
+      latest_news[link] = feed_items.pubDate;
     } else {
-      console.log("There is currently no new news!");
+      console.log("There is currently no new news!")
     }
   });
 }
 
 function news_check_loop() {
   console.log("Script started");
-  check_news();
-  setInterval(check_news, 60000);
+  media_links.forEach((link) => {
+    check_news(link);
+  })
+  setInterval(() => {
+    media_links.forEach((link) => {
+        check_news(link);
+    });
+  }, 60000);
 }
 
 news_check_loop();
